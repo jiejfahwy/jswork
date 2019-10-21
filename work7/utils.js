753772055eceb7ccdd983dd52a96b515a007e151
jiejfahwy <1465630225@qq.com>
2019-10-21 10:02:53 +0800

    work7

--- work7/utils.js
+++ work7/utils.js
@@ -9,3 +9,13 @@ function bubbleSort(str) {
     }
     return arr
   }
+  function insertSort(str) {  
+      let arr = str.split(',')
+      for(let i=1; i < arr.length;i++){
+          for(let j = i;j>0;j--){
+              if(arr[j -1]> arr[j]){
+                  [arr[j -1],arr[j]] = [arr[j], arr[j -1]]
+              }
+          }
+      }
+  }
--- work7slide/utils.js
+++ work7slide/utils.js
@@ -30,6 +30,21 @@ function bubbleSort(str) {
         if(pos[0] != -1)
         setTimeout("showLog(logValues,showDiv))", 500)
   }
+
 function inserSort(str) {  
-    
+    let arr = str.split(',')
+    let sortLog = []
+    for(let i = 1; i<arr.length; i++){
+        for(let j = 1 ;j >0;j--){
+            sortLog.push([arr.concat(), [j-1, j ]])
+            sortLog.push([arr.concat(), [j-1, j ]])
+            sortLog.push([arr.concat(), [j-1, j ]])
+            if(arr[j-1]>arr[j]){
+                [arr[j-1],arr[j]] = [arr[j], arr[j-1]]
+                sortLog.push([arr.concat(),[j,j-1]])
+            }
+        }
+    }
+    sortLog.push([arr.concat(),[-1,-1]])
+    return sortLog
 }
