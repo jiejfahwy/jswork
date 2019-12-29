a32e48cd5c23c0f870e5d112731e7f81d6f9cac0
jiejfahwy <1465630225@qq.com>
2019-12-29 18:45:25 +0800

    work20

---
+++ work20/index1.js
@@ -0,0 +1,24 @@
+// $('#toRight').click(function (){
+//     $('#right>select').append($('#left>select>option:selected'));
+// });
+// $('#toLeft').click(function (){
+//     $('#left>select').append($('#right>select>option:selected'));
+// });
+// $('#toAllRight').click(function (){
+//     $('#right>select').append($('#left>select>option'));
+// });
+// $('#toAllLeft').click(function (){
+//     $('#left>select').append($('right>select>option'));
+// });
+$('#toRight').click(function () {
+    $('#right>select').append($('#left>select>option:selected'))
+})
+$('#toLeft').click(function () {
+    $('#left>select').append($('#right>select>option:selected'))
+})
+$('#toAllRight').click(function () {
+    $('#right>select').append($('#left>select>option'))
+})
+$('#toAllLeft').click(function () {
+    $('#left>select').append($('#right>select>option'))
+})
